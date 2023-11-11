4ed20bee98e56f12758bfe4e5bcdfd1f96b7d37c
aammddkkzxc <ideal0427@naver.com>
2023-11-11 19:08:03 +0900

    feat: 방문 날짜 입력 값 숫자로 변환 기능 구현, 입력 기능 리팩터링

--- src/main/java/christmas/Date.java
+++ src/main/java/christmas/Date.java
@@ -1,6 +1,8 @@
 package christmas;
 
 public class Date {
+    public static final String DATE_RE_INPUT_REQUEST_MESSAGE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
     private final int dayNumber;
 
     public Date(int dayNumber) {
--- src/main/java/christmas/ui/InputView.java
+++ src/main/java/christmas/ui/InputView.java
@@ -1,16 +1,28 @@
 package christmas.ui;
 
+import static christmas.Date.DATE_RE_INPUT_REQUEST_MESSAGE;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String DATE_REQUEST_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
 
-    public String inputDate() {
+    public int readDate() {
         System.out.println(DATE_REQUEST_MESSAGE);
-        return Console.readLine();
+        String input = Console.readLine();
+
+        return validateReadDateNumber(input);
     }
 
-    public void inputOrder() {
+    private int validateReadDateNumber(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(DATE_RE_INPUT_REQUEST_MESSAGE);
+        }
+    }
+
+    public void readOrder() {
 
     }
 }
