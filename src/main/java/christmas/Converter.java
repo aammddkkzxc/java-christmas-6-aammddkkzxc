7ca78ab4d33c9ea4693184d5343bb13cec75a329
aammddkkzxc <ideal0427@naver.com>
2023-11-11 20:14:06 +0900

    feat: 쉼표로 구분 지어진 주문 대시로 이름과 수량구분 짓기 기능 구현

--- src/main/java/christmas/Converter.java
+++ src/main/java/christmas/Converter.java
@@ -5,8 +5,22 @@ import java.util.List;
 
 public class Converter {
     private static final String ORDER_DELIMITER = ",";
+    private static final String NAME_AND_AMOUNT_DELIMITER = "-";
 
-    public List<String> separateOrder(String input) {
-        return new ArrayList<>(List.of(input.split(ORDER_DELIMITER)));
+    public List<Order> separateNameAndAmount(String input) {
+        List<String> separatedOrder = separateOrder(input);
+        List<Order> orders = new ArrayList<>();
+
+        for (String nameAndAmount : separatedOrder) {
+            List<String> separatedNameAndAmount = new ArrayList<>(
+                    List.of(nameAndAmount.split(NAME_AND_AMOUNT_DELIMITER)));
+            orders.add(new Order(separatedNameAndAmount.get(0), separatedNameAndAmount.get(1)));
+        }
+
+        return orders;
+    }
+
+    public List<String> separateOrder(String order) {
+        return new ArrayList<>(List.of(order.split(ORDER_DELIMITER)));
     }
 }
