7b3fb7186fbd26321a26fb4e56dc5d1bbad6da7d
aammddkkzxc <ideal0427@naver.com>
2023-11-11 19:46:32 +0900

    feat: 주문 입력 기능 구현

--- src/main/java/christmas/ui/InputView.java
+++ src/main/java/christmas/ui/InputView.java
@@ -4,12 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String DATE_REQUEST_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String ORDER_REQUEST_MESSAGE
+            = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     public String readDate() {
         System.out.println(DATE_REQUEST_MESSAGE);
         return Console.readLine();
     }
 
-    public void readOrder() {
+    public String readOrder() {
+        System.out.println(ORDER_REQUEST_MESSAGE);
+        return Console.readLine();
     }
 }
