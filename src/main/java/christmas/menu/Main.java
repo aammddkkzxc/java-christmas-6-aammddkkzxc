b22b889bc0d1a2406db53169a755fd395a8d7a3b
aammddkkzxc <ideal0427@naver.com>
2023-11-11 18:42:34 +0900

    feat: 메뉴와 메뉴타입 열거로 만들어서 메뉴판 기능 구현

--- src/main/java/christmas/menu/Main.java
+++
@@ -1,4 +0,0 @@
-package christmas.menu;
-
-public enum Main {
-}
---
+++ src/main/java/christmas/menutable/MenuType.java
@@ -0,0 +1,27 @@
+package christmas.menutable;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum MenuType {
+    APPETIZER("애피타이저", Arrays.asList(Menu.MUSHROOM_SOUP, Menu.TAPAS, Menu.CAESAR_SALAD)),
+    MAIN("메인", Arrays.asList(Menu.T_BONE_STAKE, Menu.BARBECUE_LIP, Menu.SEAFOOD_PASTA, Menu.CHRISTMAS_PASTA)),
+    DESSERT("디저트", Arrays.asList(Menu.CHOCOLATE_CAKE, Menu.ICE_CREAM)),
+    BEVERAGE("음료", Arrays.asList(Menu.ZERO_COKE, Menu.RED_WINE, Menu.CHAMPAGNE));
+
+    private final String name;
+    private final List<Menu> menus;
+
+    MenuType(String name, List<Menu> menus) {
+        this.name = name;
+        this.menus = menus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Menu> getMenus() {
+        return menus;
+    }
+}
