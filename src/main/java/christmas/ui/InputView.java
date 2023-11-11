71549fa8098cc7891d3c741720e1c2439e0f09a7
aammddkkzxc <ideal0427@naver.com>
2023-11-11 19:44:18 +0900

    refactor: 방문 날짜 유효성 검사 기능 클래스 위치 변경 및 리팩터링

--- src/main/java/christmas/ui/InputView.java
+++ src/main/java/christmas/ui/InputView.java
@@ -1,28 +1,15 @@
 package christmas.ui;
 
-import static christmas.Date.DATE_RE_INPUT_REQUEST_MESSAGE;
-
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String DATE_REQUEST_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
 
-    public int readDate() {
+    public String readDate() {
         System.out.println(DATE_REQUEST_MESSAGE);
-        String input = Console.readLine();
-
-        return validateReadDateNumber(input);
-    }
-
-    private int validateReadDateNumber(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(DATE_RE_INPUT_REQUEST_MESSAGE);
-        }
+        return Console.readLine();
     }
 
     public void readOrder() {
-
     }
 }
