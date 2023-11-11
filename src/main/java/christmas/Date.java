4d77279fd32696b77c81ae08e15c501c4ffd21f6
aammddkkzxc <ideal0427@naver.com>
2023-11-11 19:18:12 +0900

    feat: 방문 날짜 1~31 사이의 숫자 유효성 검사 기능 구현

--- src/main/java/christmas/Date.java
+++ src/main/java/christmas/Date.java
@@ -2,11 +2,21 @@ package christmas;
 
 public class Date {
     public static final String DATE_RE_INPUT_REQUEST_MESSAGE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final int DATE_MIN_NUMBER = 1;
+    private static final int DATE_MAX_NUMBER = 31;
 
     private final int dayNumber;
 
     public Date(int dayNumber) {
+        validateRange(dayNumber);
+
         this.dayNumber = dayNumber;
+    }
+
+    private void validateRange(int dayNumber) {
+        if ((dayNumber < DATE_MIN_NUMBER) || (dayNumber > DATE_MAX_NUMBER)) {
+            throw new IllegalArgumentException(DATE_RE_INPUT_REQUEST_MESSAGE);
+        }
     }
 
     public boolean isUntilChristmas() {
