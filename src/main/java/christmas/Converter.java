0d1ab4f6260c1d298bf29256d1d8d05da72db93c
aammddkkzxc <ideal0427@naver.com>
2023-11-11 20:43:29 +0900

    refactor: 대시로 주문 메뉴와 해당 매뉴 개수 구분 짓는 기능 분리

--- src/main/java/christmas/Converter.java
+++ src/main/java/christmas/Converter.java
@@ -1,6 +1,6 @@
 package christmas;
 
-import static christmas.Date.DATE_RE_INPUT_REQUEST_MESSAGE;
+import static christmas.Date.DATE_RE_READ_REQUEST_MESSAGE;
 import static christmas.Order.ORDER_RE_READ_REQUEST_MESSAGE;
 
 import java.util.ArrayList;
@@ -14,30 +14,35 @@ public class Converter {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(DATE_RE_INPUT_REQUEST_MESSAGE);
+            throw new IllegalArgumentException(DATE_RE_READ_REQUEST_MESSAGE);
         }
     }
 
-    public List<Order> separateNameAndAmount(String input) {
-        List<String> separatedOrder = separateOrder(input);
+    /*private static List<Order> makeOrders(String readOrder) {
+        List<String> separatedOrder = separateOrder(readOrder);
         List<Order> orders = new ArrayList<>();
 
-        for (String nameAndAmount : separatedOrder) {
-            List<String> separatedNameAndAmount = new ArrayList<>(
-                    List.of(nameAndAmount.split(NAME_AND_AMOUNT_DELIMITER)));
-            orders.add(new Order(separatedNameAndAmount.get(0), separatedNameAndAmount.get(1)));
+        for (String order : separatedOrder) {
+            List<String> separatedNameAndAmount = separateNameAndAmount(order);
+            String name = separatedNameAndAmount.get(0);
+            int amount = convertAmount(separatedNameAndAmount.get(1));
+            orders.add(new Order(name, amount));
         }
 
         return orders;
+    }*/
+
+    private static List<String> separateOrder(String readOrder) {
+        return new ArrayList<>(List.of(readOrder.split(ORDER_DELIMITER)));
+    }
+    
+    private static List<String> separateNameAndAmount(String order) {
+        return new ArrayList<>(List.of(order.split(NAME_AND_AMOUNT_DELIMITER)));
     }
 
-    public List<String> separateOrder(String order) {
-        return new ArrayList<>(List.of(order.split(ORDER_DELIMITER)));
-    }
-
-    private static int convertAmount(String input) {
+    private static int convertAmount(String amount) {
         try {
-            return Integer.parseInt(input);
+            return Integer.parseInt(amount);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ORDER_RE_READ_REQUEST_MESSAGE);
         }
