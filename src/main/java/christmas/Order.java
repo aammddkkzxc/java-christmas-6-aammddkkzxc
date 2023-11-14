7e5851ecf355f44da74f43acda3007489b7ca427
aammddkkzxc <ideal0427@naver.com>
2023-11-14 19:09:21 +0900

    feat: 1 이상의 주문 수량 유효성 검사 기능 구현

--- src/main/java/christmas/Order.java
+++ src/main/java/christmas/Order.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 
 public class Order {
     public static final String ORDER_RE_READ_REQUEST_MESSAGE = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final int MINIMUM_QUANTITY = 1;
 
     private final String name;
     private final int amount;
@@ -13,12 +14,20 @@ public class Order {
 
     public Order(String name, int amount) {
         validateOrderNameExistInMenu(name);
+        validateOrderAmountBiggerThanMinimumQuantity(amount);
+
         this.name = name;
         this.amount = amount;
     }
 
     private void validateOrderNameExistInMenu(String name) {
         if (Menu.decideMenu(name) == Menu.NONE) {
+            throw new IllegalArgumentException(ORDER_RE_READ_REQUEST_MESSAGE);
+        }
+    }
+
+    private void validateOrderAmountBiggerThanMinimumQuantity(int amount) {
+        if (amount < MINIMUM_QUANTITY) {
             throw new IllegalArgumentException(ORDER_RE_READ_REQUEST_MESSAGE);
         }
     }
