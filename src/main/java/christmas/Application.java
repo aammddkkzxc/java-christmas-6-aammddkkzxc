90b439844e50aa109f9baff33577dc7e10e76e18
aammddkkzxc <ideal0427@naver.com>
2023-11-14 15:44:30 +0900

    refactor: ReservationStage의 날짜 데이터 생성하고 검증하는 로직 InputView로 옮기고 통합

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -3,14 +3,14 @@ package christmas;
 import christmas.programstages.EventDecisionStage;
 import christmas.programstages.ReservationStage;
 import christmas.programstages.ResultStage;
+import christmas.ui.InputView;
 import java.util.List;
 import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        ReservationStage reservationStage = new ReservationStage();
-        Date date = reservationStage.runMakeDateStage();
+        Date date = InputView.makeValidatedDate();
         List<Order> orders = reservationStage.runMakeOrdersStage();
         EventDecisionStage eventDecisionStage = new EventDecisionStage(date, orders);
         int totalOrderPrice = eventDecisionStage.calculateTotalOrderPrice();
--- src/main/java/christmas/ui/InputView.java
+++ src/main/java/christmas/ui/InputView.java
@@ -1,15 +1,29 @@
 package christmas.ui;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.Converter;
+import christmas.Date;
 
 public class InputView {
     private static final String DATE_REQUEST_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String ORDER_REQUEST_MESSAGE
             = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
-    public static String readDate() {
+    public static Date makeValidatedDate() {
+        try {
+            return makeDate();
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e);
+            return makeValidatedDate();
+        }
+    }
+
+    private static Date makeDate() {
         System.out.println(DATE_REQUEST_MESSAGE);
-        return Console.readLine();
+        String dateInput = Console.readLine();
+        int dayNumber = Converter.convertDateInput(dateInput);
+
+        return new Date(dayNumber);
     }
 
     public static String readOrder() {
