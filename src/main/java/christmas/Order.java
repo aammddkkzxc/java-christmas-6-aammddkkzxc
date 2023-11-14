cf3aadf1d9019482e0d9aab6a7e0d3d1333d75e1
aammddkkzxc <ideal0427@naver.com>
2023-11-14 17:41:14 +0900

    feat: 메뉴판에 존재하지 않는 값을 메뉴로 입력할 경우 예외 발생 기능 구현

--- src/main/java/christmas/Order.java
+++ src/main/java/christmas/Order.java
@@ -4,7 +4,7 @@ import christmas.menutable.Menu;
 import java.util.Objects;
 
 public class Order {
-    public static final String ORDER_RE_READ_REQUEST_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    public static final String ORDER_RE_READ_REQUEST_MESSAGE = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
     private final String name;
     private final int amount;
@@ -13,6 +13,12 @@ public class Order {
     public Order(String name, int amount) {
         this.name = name;
         this.amount = amount;
+    }
+
+    private void validateOrderNameExistInMenu(String name) {
+        if(Menu.decideMenu(name) == Menu.NONE) {
+            throw new IllegalArgumentException(ORDER_RE_READ_REQUEST_MESSAGE);
+        }
     }
 
     public int calculatePrice() {
