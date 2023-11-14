eb4f9f1e22b3f496f19022d756668dce9558a0e7
aammddkkzxc <ideal0427@naver.com>
2023-11-14 20:29:51 +0900

    refactor: 날짜 평일 판단 기능 조건 상수화

--- src/main/java/christmas/Date.java
+++ src/main/java/christmas/Date.java
@@ -1,7 +1,13 @@
 package christmas;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Date {
     public static final String DATE_RE_READ_REQUEST_MESSAGE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final List<Integer> WEEKDAY_CONDITION = new ArrayList<>(Arrays.asList(0, 3, 4, 5, 6));
+    private static final int DAY_NUMBER_JUDGEMENT_FACTOR = 7;
     private static final int DATE_MIN_NUMBER = 1;
     private static final int DATE_MAX_NUMBER = 31;
 
@@ -23,8 +29,7 @@ public class Date {
     }
 
     public boolean isWeekday() {
-        return (dayNumber % 7 == 0) || (dayNumber % 7 == 3) || (dayNumber % 7 == 4) || (dayNumber % 7 == 5) || (
-                dayNumber % 7 == 6);
+        return WEEKDAY_CONDITION.contains(dayNumber % DAY_NUMBER_JUDGEMENT_FACTOR);
     }
 
     public boolean isWeekend() {
