14b1f7367549e63a0989cab63c5a2384865cfc92
aammddkkzxc <ideal0427@naver.com>
2023-11-15 22:03:30 +0900

    test: 총 주문 메뉴의 개수가 20개 초과하면 예외 발생하는 기능 테스트 코드 구현

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -32,6 +32,22 @@ class OrderTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("총 주문 메뉴들의 개수가 20개를 초과하면 예외가 발생한다.")
+    @Test
+    void createOrderByOverQuantityLimit() {
+        List<OrderedMenu> orderedMenus = new ArrayList<>();
+        orderedMenus.add(new OrderedMenu("타파스", 3));
+        orderedMenus.add(new OrderedMenu("양송이수프", 2));
+        orderedMenus.add(new OrderedMenu("레드와인", 3));
+        orderedMenus.add(new OrderedMenu("제로콜라", 5));
+        orderedMenus.add(new OrderedMenu("바비큐립", 4));
+        orderedMenus.add(new OrderedMenu("아이스크림", 4));
+
+
+        assertThatThrownBy(() -> new Order(orderedMenus))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @Test
     void calculateTotalOrderPrice() {
     }
