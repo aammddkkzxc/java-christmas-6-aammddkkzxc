4f0b8160236c9cffee61c679f2d4f8ae56d36077
aammddkkzxc <ideal0427@naver.com>
2023-11-15 20:19:59 +0900

    test: 이벤트에 충분하지 않은 전체 주문 금액일 경우 전체 혜택 도출 기능 테스트 코드 구현

--- src/test/java/christmas/domain/EventProcessTest.java
+++ src/test/java/christmas/domain/EventProcessTest.java
@@ -1,19 +1,15 @@
 package christmas.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 
 class EventProcessTest {
-
-    @DisplayName("이벤트 받는 금액에 충분한 전체 주문 금액으로 전체 혜택 계산")
+    @DisplayName("이벤트에 충분한 전체 주문 금액으로 전체 혜택 도출")
     @Test
     void takeAllBenefitWithSufficientTotalOrderPrice() {
         Date dateInput = new Date(3);
@@ -34,6 +30,30 @@ class EventProcessTest {
         orderedMenus.add(new OrderedMenu("해산물파스타", 2));
         orderedMenus.add(new OrderedMenu("레드와인", 1));
         orderedMenus.add(new OrderedMenu("초코케이크", 1));
+
+        return orderedMenus;
+    }
+
+    @DisplayName("이벤트에 충분하지 않은 전체 주문 금액으로 전체 혜택 도출")
+    @Test
+    void takeAllBenefitWithInSufficientTotalOrderPrice() {
+        Date dateInput = new Date(26);
+        Order orderInput = new Order(setUpInSufficientTotalOrderPriceOrder());
+        EventProcess eventProcess = new EventProcess();
+        EventResult eventResult = eventProcess.takeAllBenefit(dateInput, orderInput);
+        Map<BenefitType, Integer> result = eventResult.getAllBenefit();
+
+        assertThat(result.get(BenefitType.GIFT)).isEqualTo(0);
+        assertThat(result.get(BenefitType.D_DAY)).isEqualTo(0);
+        assertThat(result.get(BenefitType.WEEKDAY)).isEqualTo(0);
+        assertThat(result.get(BenefitType.WEEKEND)).isEqualTo(0);
+        assertThat(result.get(BenefitType.SPECIAL)).isEqualTo(0);
+    }
+
+    private List<OrderedMenu> setUpInSufficientTotalOrderPriceOrder() {
+        List<OrderedMenu> orderedMenus = new ArrayList<>();
+        orderedMenus.add(new OrderedMenu("타파스", 1));
+        orderedMenus.add(new OrderedMenu("제로콜라", 1));
 
         return orderedMenus;
     }
