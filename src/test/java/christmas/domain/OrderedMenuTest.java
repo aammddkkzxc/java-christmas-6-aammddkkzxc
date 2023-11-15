ab62b7da3d6dcf439f89ab6211c4e506d80aebbb
aammddkkzxc <ideal0427@naver.com>
2023-11-15 22:19:51 +0900

    test: 메뉴에 존재하지 않는 주문메뉴 일시 예외 발생 기능 테스트 코드 예시 추가

--- src/test/java/christmas/domain/OrderedMenuTest.java
+++ src/test/java/christmas/domain/OrderedMenuTest.java
@@ -5,12 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class OrderedMenuTest {
     @DisplayName("메뉴에 존재하지 않는 주문메뉴이면 예외가 발생한다.")
-    @Test
-    void createOrderedMenuWithNonExistingMenu() {
-        assertThatThrownBy(() -> new OrderedMenu("피자", 3))
+    @ValueSource(strings = {"피자", "치킨", "햄버거"})
+    @ParameterizedTest
+    void createOrderedMenuWithNonExistingMenu(String input) {
+        assertThatThrownBy(() -> new OrderedMenu(input, 3))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
