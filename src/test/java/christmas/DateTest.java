9e852897f3e1f9eca751c4153ee7967fa32bc406
aammddkkzxc <ideal0427@naver.com>
2023-11-15 19:22:57 +0900

    test: 주말 할인 판단 기능 테스트 코드 구현

--- src/test/java/christmas/DateTest.java
+++ src/test/java/christmas/DateTest.java
@@ -11,7 +11,7 @@ class DateTest {
 
     @BeforeEach
     void setUP() {
-        date = new Date(25);
+        date = new Date(24);
     }
 
     @DisplayName("주어진 날짜가 크리스마스 이전일 경우 디데이 할인에 해당한다.")
@@ -22,11 +22,19 @@ class DateTest {
         assertThat(result).isEqualTo(true);
     }
 
-    @DisplayName("주어진 날짜가 평일일 경우 평일 할인에 해당한다.")
+    @DisplayName("주어진 날짜가 평일로 정의한 날에 해당할 경우 평일 할인에 해당한다.")
     @Test
     void isWeekdayDiscountActive() {
         boolean result = date.isWeekdayDiscountActive();
 
         assertThat(result).isEqualTo(true);
+    }
+
+    @DisplayName("주어진 날짜가 주말로 정의한 날에 해당할 경우 주말 할인에 해당한다.")
+    @Test
+    void isWeekendDiscountActive() {
+        boolean result = date.isWeekendDiscountActive();
+
+        assertThat(result).isEqualTo(false);
     }
 }
